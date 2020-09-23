bc703ed16d3e5dfa05751fbc606fc8b607ebe861
tuanpth1909 <60849085+tuanpth1909@users.noreply.github.com>
2020-09-23 15:34:06 +0700

    Add project files.

---
+++ js/validate.js
@@ -0,0 +1,44 @@
+﻿$(document).ready(function () {
+    alert("hello world");
+
+    //Validate form 
+    $('#validate').validate({
+        rules: {
+            email: {
+                required: true,
+                email: true
+            },
+            password: {
+                required: true,
+                rangelength: [8, 16]
+            },
+            address1: {
+                required: true
+            },
+            zip: {
+                required: true,
+                length: 6,
+                number: true,
+            },
+        },
+        messages: {
+            email: {
+                required: "Vui lòng nhập email!",
+                email: "Định dạng phải là email",
+            },
+            password: {
+                required: "Vui lòng nhập password!",
+                rangelength: "Độ dài của password từ 8 đến 16 kí tự",
+            },
+            address1: {
+                required: "Vui lòng nhập địa chỉ!",
+            },
+            zip: {
+                required: "Vui lòng nhập mã zip",
+                length: "Chỉ 6 kí tự!",
+                number: "Mã zip chỉ có thể là số!",
+            },
+        }
+    });
+
+});
